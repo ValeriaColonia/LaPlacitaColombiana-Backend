e2e62a2267b91866da72af09387efc8e00289d8d
estebangccoder <estebangccoder@gmail.com>
2025-09-22 14:34:30 -0500

    Modificacion de entidad productor

--- src/main/java/com/laplacitacolombiana/springboot/model/Proveedor.java
+++ src/main/java/com/laplacitacolombiana/springboot/model/Proveedor.java
@@ -1,5 +1,6 @@
 package com.laplacitacolombiana.springboot.model;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -9,52 +10,77 @@ public class Proveedor {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long idProveedor;
+
+    @NotBlank(message = "La razón social es obligatorio")
+    @Size(min = 2, max = 100, message = "La razón social debe tener entre 2 y 100 caracteres")
+    private String razonSocial;
 
     @NotBlank(message = "El nombre es obligatorio")
     @Size(min = 2, max = 100, message = "El nombre debe tener entre 2 y 100 caracteres")
-    private String nombre;
+    private String nombreProveedor;
 
     @NotBlank(message = "El teléfono es obligatorio")
     @Pattern(
             regexp = "^[+\\d][\\d\\s\\-()]{6,19}$",
             message = "El teléfono contiene un formato inválido")
-    private String telefono;
+    private String telefonoProveedor;
+
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "Formato de email inválido")
+    private String emailProveedor;
 
     @NotBlank(message = "La dirección es obligatoria")
     @Size(max = 150, message = "La dirección no debe exceder 150 caracteres")
-    private String direccion;
+    private String direccionProveedor;
 
     public Long getId() {
-        return id;
+        return idProveedor;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setId(Long idProveedor) {
+        this.idProveedor = idProveedor;
     }
 
-    public String getNombre() {
-        return nombre;
+
+    public String getRazonSocial() {
+        return razonSocial;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
     }
 
-    public String getTelefono() {
-        return telefono;
+    public String getNombreProveedor() {
+        return nombreProveedor;
     }
 
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
+    public void setNombreProveedor(String nombreProveedor) {
+        this.nombreProveedor = nombreProveedor;
     }
 
-    public String getDireccion() {
-        return direccion;
+    public String getTelefonoProveedor() {
+        return telefonoProveedor;
     }
 
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
+    public void setTelefonoProveedor(String telefonoProveedor) {
+        this.telefonoProveedor = telefonoProveedor;
+    }
+
+    public String getEmailProveedor() {
+        return emailProveedor;
+    }
+
+    public void setEmailProveedor(String emailProveedor) {
+        this.emailProveedor = emailProveedor;
+    }
+
+    public String getDireccionProveedor() {
+        return direccionProveedor;
+    }
+
+    public void setDireccionProveedor(String direccionProveedor) {
+        this.direccionProveedor = direccionProveedor;
     }
 }
 
