af419826871aa581c46ec5bf7e454825088e1df8
jeinerdelgado <jeinerdelgadov@gmail.com>
2025-09-22 12:38:05 -0500

    arreglo para ver usuarios en el front

--- src/main/java/com/laplacitacolombiana/springboot/repository/RolRepository.java
+++ src/main/java/com/laplacitacolombiana/springboot/repository/RolRepository.java
@@ -1,8 +1,11 @@
+
 package com.laplacitacolombiana.springboot.repository;
 
 import com.laplacitacolombiana.springboot.model.Rol;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RolRepository extends JpaRepository<Rol, Long> {
-}
+import java.util.Optional;
 
+public interface RolRepository extends JpaRepository<Rol, Long> {
+    Optional<Rol> findByNombre(String nombre);
+}
